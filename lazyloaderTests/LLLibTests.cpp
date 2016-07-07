49fc9bef91b4e00b837211d708122c1e9c84c03f
Knyazev Sergey <yaknyazsergei@gmail.com>
2016-07-07 15:47:33 +0300

    Добавлены тесты конструктора копирования

--- lazyloaderTests/LLLibTests.cpp
+++ lazyloaderTests/LLLibTests.cpp
@@ -3,17 +3,20 @@
 #include "../liblazyloader/Image.h"
 #include "../liblazyloader/GdiPlus.h"
 BOOST_AUTO_TEST_SUITE(Check_class_CImage)
+CGdiPlus gdiInit;
 BOOST_AUTO_TEST_CASE(constructor_wchar)
 {
 	CGdiPlus gdiInit;
 	BOOST_CHECK_NO_THROW(lload::CImage(L"./images/0.png"));
 }
+
 BOOST_AUTO_TEST_CASE(copy_constructor)
 {
-	//lload::CImage original(L"./images/0.png");
-	//lload::CImage copy(original);
-	//BOOST_CHECK_EQUAL(original.GetFileName(), copy.GetFileName());
-	//BOOST_CHECK_EQUAL(original.GetBitmap().GetWidth(), copy.GetBitmap().GetWidth());
-	//BOOST_CHECK_EQUAL(original.GetBitmap().GetHeight(), copy.GetBitmap().GetWidth());
+	lload::CImage original(L"./images/0.png");
+	lload::CImage copy(original);
+	BOOST_CHECK_EQUAL(original.GetFileName().c_str(), copy.GetFileName().c_str());
+	BOOST_CHECK_EQUAL(original.GetBitmap().GetWidth(), copy.GetBitmap().GetWidth());
+	BOOST_CHECK_EQUAL(original.GetBitmap().GetHeight(), copy.GetBitmap().GetWidth());
+	BOOST_CHECK_EQUAL(original.GetBitmap().GetPixelFormat(), copy.GetBitmap().GetPixelFormat());
 }
 BOOST_AUTO_TEST_SUITE_END()
