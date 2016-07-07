557a4dcafadf784d5a7c2c46ae3b5b8d21ce63d0
Knyazev Sergey <yaknyazsergei@gmail.com>
2016-07-07 15:40:03 +0300

    Добавлен тест кейс проверки конструктора CImage

--- lazyloaderTests/LLLibTests.cpp
+++ lazyloaderTests/LLLibTests.cpp
@@ -1,10 +1,11 @@
 #include "stdafx.h"
 
 #include "../liblazyloader/Image.h"
-
+#include "../liblazyloader/GdiPlus.h"
 BOOST_AUTO_TEST_SUITE(Check_class_CImage)
 BOOST_AUTO_TEST_CASE(constructor_wchar)
 {
+	CGdiPlus gdiInit;
 	BOOST_CHECK_NO_THROW(lload::CImage(L"./images/0.png"));
 }
 BOOST_AUTO_TEST_CASE(copy_constructor)
