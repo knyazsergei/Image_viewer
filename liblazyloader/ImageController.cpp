c5e6d35b2b8666cdd6c2ec76a53b9ae348433602
Knyazev Sergey <yaknyazsergei@gmail.com>
2016-07-08 10:55:38 +0300

    Вывод контроллера в консоль изменен

--- liblazyloader/ImageController.cpp
+++ liblazyloader/ImageController.cpp
@@ -7,9 +7,12 @@ lload::CImageController::CImageController()
 	m_imageLoader = std::make_unique<CImageLoader>(L"./images/");
 	m_imageLoader->SetImagePageSize(m_pageSize);
 
+	//load first page
 	m_collections.push_back({});
 	m_imageLoader->Load(m_collections.back(), 0);
 	m_currentCollection = m_collections.begin();
+	m_currentCollectionIndex = 0;
+	UpdateCollections(false);//load next page
 }
 
 bool lload::CImageController::NeedUpdate()
@@ -19,37 +22,24 @@ bool lload::CImageController::NeedUpdate()
 
 void lload::CImageController::NextPage()
 {
-	if (m_currentCollectionIndex == m_collections.size() - 1)
+	if(m_currentCollection != m_collections.end())
 	{
-		m_currentCollectionIndex = 0;
-		m_currentCollection = std::begin(m_collections);
-	}
-	else if(m_collections.size() != 0)
-	{
+		
 		m_currentCollectionIndex++;
 		m_currentCollection++;
+		
+		UpdateCollections(false);
 	}
-
-	UpdateCollections(false);
 }
 
 void lload::CImageController::PrevPage()
 {
-	if (m_currentCollectionIndex == 0)
-	{
-		if (m_collections.size() != 0)
-		{
-			m_currentCollectionIndex = m_collections.size() - 1;
-		}
-		m_currentCollection = std::end(m_collections);
-	}
-	else
+	if (m_currentCollectionIndex > 0)
 	{
 		m_currentCollectionIndex--;
 		m_currentCollection--;
-	}
-	
-	UpdateCollections(true);
+		UpdateCollections(true);
+	}	
 }
 
 lload::CImageCollection & lload::CImageController::GetPage()
@@ -61,34 +51,35 @@ void lload::CImageController::UpdateCollections(bool left)
 {
 	if (left)
 	{
+		//load left page
 		if (m_currentCollectionIndex > 0)
 		{
 			m_collections.push_front({});
 			m_imageLoader->Load(m_collections.front(), (m_currentCollectionIndex - 1) * m_pageSize);
-			if (m_collections.size() > 3)
+			if (m_collections.size() > 3 && m_currentCollectionIndex != 0)
 			{
 				m_collections.pop_back();
 			}
-			else if (m_collections.size() == 1)
-			{
-				m_currentCollection = m_collections.begin();
-			}
 		}
 	}
-	else
+	else if (m_currentCollectionIndex * m_pageSize < m_imageLoader->GetFileNumber())
 	{
-		if (m_currentCollectionIndex * m_pageSize < m_imageLoader->GetFileNumber())
+		std::cout << m_currentCollectionIndex * m_pageSize << " " << m_imageLoader->GetFileNumber() << '\n';
+		//load right page
+		m_collections.push_back({});
+		try
 		{
-			m_collections.push_back({});
 			m_imageLoader->Load(m_collections.back(), (m_currentCollectionIndex + 1) * m_pageSize);
-			if (m_collections.size() > 3)
-			{
-				m_collections.pop_front();
-			}
-			else if (m_collections.size() == 1)
-			{
-				m_currentCollection = m_collections.begin();
-			}
+		}
+		catch (...)
+		{
+			m_collections.pop_back();
+		}
+		
+
+		if (m_collections.size() > 3)
+		{
+			m_collections.pop_front();
 		}
 	}
 }
